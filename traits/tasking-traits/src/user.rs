08e0b57c2cbc0760231037e99395b4718fa0fd95
Rajat Petwal <rajatpetwal67@gmail.com>
2023-04-18 08:39:58 +0530

    rating module changed: rating in 10 power of 3

--- traits/tasking-traits/src/user.rs
+++ traits/tasking-traits/src/user.rs
@@ -11,9 +11,13 @@ pub struct User {
 	pub name: Vec<u8>,
 	pub email: Vec<u8>,
 	pub phone_number: Vec<u8>,
-	rating_as_worker: Option<u8>,
-	rating_as_publisher: Option<u8>,
-	overall_rating: Option<u8>,
+	total_work_as_worker: u32,
+
+	// rating is saved in 10 power 3
+	rating_as_worker: Option<u32>,
+	total_work_as_publisher: u32,
+	rating_as_publisher: Option<u32>,
+	overall_rating: Option<u32>,
 	// tags
 	tags: BTreeSet<Vec<u8>>,
 }
@@ -24,7 +28,9 @@ impl Default for User {
 			name: Vec::new(),
 			email: Vec::new(),
 			phone_number: Vec::new(),
+			total_work_as_worker: 0,
 			rating_as_worker: None,
+			total_work_as_publisher: 0,
 			rating_as_publisher: None,
 			overall_rating: None,
 			tags: BTreeSet::new(),
@@ -35,25 +41,41 @@ impl Default for User {
 impl User {
 	pub fn update_worker_rating(&mut self, new_rating: u8) {
 		match self.rating_as_worker {
-			None => self.rating_as_worker = Some(new_rating),
-			Some(rating) => self.rating_as_worker = Some((rating + new_rating) / 2),
+			None => {
+				self.total_work_as_worker += 1;
+				self.rating_as_worker = Some(new_rating as u32 * 1000);
+			},
+			Some(rating) => {
+				let total_rating = self.total_work_as_worker * rating + new_rating as u32 * 1000;
+				self.total_work_as_worker += 1;
+				let avg_rating = total_rating / self.total_work_as_worker;
+				self.rating_as_worker = Some(avg_rating);
+			},
 		}
 		self.update_overall_rating();
 	}
 
 	pub fn update_publisher_rating(&mut self, new_rating: u8) {
 		match self.rating_as_publisher {
-			None => self.rating_as_publisher = Some(new_rating),
-			Some(rating) => self.rating_as_publisher = Some((rating + new_rating) / 2),
+			None => {
+				self.total_work_as_publisher += 1;
+				self.rating_as_publisher = Some(new_rating as u32 * 1000);
+			},
+			Some(rating) => {
+				let total_rating = self.total_work_as_publisher * rating + new_rating as u32 * 1000;
+				self.total_work_as_publisher += 1;
+				let avg_rating = total_rating / self.total_work_as_publisher;
+				self.rating_as_publisher = Some(avg_rating);
+			},
 		}
 		self.update_overall_rating();
 	}
 
-	pub fn get_worker_rating(&self) -> Option<u8> {
+	pub fn get_worker_rating(&self) -> Option<u32> {
 		self.rating_as_worker
 	}
 
-	pub fn get_publisher_rating(&self) -> Option<u8> {
+	pub fn get_publisher_rating(&self) -> Option<u32> {
 		self.rating_as_publisher
 	}
 
